bd0c1af1557719e1dd9da8119e1783036164ba10
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-07-02 16:58:14 +0700

    Added punctuation replacement function

--- helpers/mongoose-helper.js
+++ helpers/mongoose-helper.js
@@ -1,14 +1,22 @@
 function enumerateModelError(error) {
-	// takes an error object returned by save method of a model object
-	// returns an array of each path of a model which contains error
+  // takes an error object returned by save method of a model object
+  // returns an array of each path of a model which contains error
 
-	var tmpArrayOfError = [];
+  var tmpArrayOfError = [];
 
-	for (item in error.errors) {
-	  tmpArrayOfError.push(error.errors[item]);
-	}
+  for (item in error.errors) {
+    tmpArrayOfError.push(error.errors[item]);
+  }
 
-	return tmpArrayOfError;
+  return tmpArrayOfError;
+}
+
+function replacePunctuation(value, replacement) {
+  var regex = /['!"#$%&\\'()\*+\-\.\/:;<=>?@\[\\\]\^_`{|}~'\ ]/g;
+  var newValue = value.replace(regex, replacement);
+
+  return newValue
 }
 
 exports.enumerateModelError = enumerateModelError;
+exports.replacePunctuation = replacePunctuation;
