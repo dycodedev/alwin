2d65dfc56ee9b7409c155e8d9b9ef961740f3a75
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-07-02 15:58:36 +0700

    Modified labels nama path validation

--- models/label.js
+++ models/label.js
@@ -3,11 +3,14 @@ var mongoose = require('mongoose');
 var namaMaxLength = [20, 'Path {PATH} exceeds the maximum allowed length'];
 
 function validateNamaLabel(value) {
-  return /[a-zA-Z]{1, 20}/.test(value);
+  return /([a-zA-Z]+)/.test(value);
 }
 
+var namaLabelCustomValidator = [validateNamaLabel, 'Label name is not valid'];
+
 var LabelSchema = new mongoose.Schema({
-  nama: { type: String, maxlength: namaMaxLength, validate: validateNamaLabel }
+  nama: { type: String, maxlength: namaMaxLength, validate: namaLabelCustomValidator }
 });
 
-module.exports = mongoose.model('Label', LabelSchema);
+exports.LabelModel = mongoose.model('Label', LabelSchema);
+exports.LabelSchema = LabelSchema;
