e404c7bf0cec214fc4aae8a51515415fd93a5d8d
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-07-02 13:17:02 +0700

    Added Post and Komentar models

---
+++ models/komentar.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+function matchEmail(value) {
+  var re = /([a-zA-Z\_\.0-9]+)@([a-zA-Z\_\.0-9]+)\.([a-zA-Z\_\.0-9]{1,4})/;
+  return re.test(value);
+}
+
+var emailCustomValidator = [matchEmail, 'Email is not valid'];
+
+var maxLengthErrorMessage = 'Path {PATH} exceeds the maximum allowed length';
+
+var namaMaxLength = [50, maxLengthErrorMessage];
+var isiMaxLength = [600, maxLengthErrorMessage];
+
+var KomentarSchema = new mongoose.KomentarSchema({
+  nama: { type: String, default: 'Anonymous', maxlength: namaMaxLength },
+  email: { type: String, required: true, validate: emailCustomValidator },
+  isi: { type: String, required: true, maxlength: isiMaxLength },
+  tanggal: { type: Date, required: true, default: Date.now }
+});
+
+module.exports = mongoose.model('komentar', KomentarSchema);
---
+++ models/post.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Komentar = require('./kometar');
+
+var PostSchema = new mongoose.Schema({
+  judul: { type: String, required: true, maxlength: 100 },
+  konten: { type: String, required: true },
+  tanggal: { type: Date, required: true, default: Date.now },
+  komentar: [Komentar],
+  dibaca: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
